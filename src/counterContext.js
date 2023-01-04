3be5bc252836ba7e3db7ceb239dae3f6cd214d9a
priyapandey28 <121967472+priyapandey28@users.noreply.github.com>
2023-01-05 00:09:06 +0530

    Add files via upload

---
+++ src/counterContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from "react";
+
+
+const counterContext = createContext()
+
+
+const CounterStateContext = (props) => {
+    // let count = 0;
+    // let setCount = ()=>{
+    //     count++;
+    // }
+    const [count, setCount] = useState(0);
+    
+    return (
+        <div id="counter-context">
+            <counterContext.Provider value={{ count, setCount }}>
+                {props.children}
+            </counterContext.Provider >
+        </div>
+
+    )
+}
+
+export { counterContext, CounterStateContext }
+
